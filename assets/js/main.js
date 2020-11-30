12af94232e6ee43cc226f3bd7c38eb4f567fd58c
edidk7 <d.ignacio292@gmail.com>
2020-11-29 21:16:06 -0300

    fix header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
-    AOS.init({
-        duration: 1200,
-    });
+  AOS.init({
+    duration: 1200,
+  });
   // Inicializando el carrousel para dispositivos móviles
   $("#owl-carousel-responsive-home").owlCarousel({
     navigation: true, // Show next and prev buttons
@@ -22,12 +22,20 @@ $(document).ready(function () {
     animateIn: "fadeIn",
     autoHeight: true,
   });
+
+  $(window).scroll(function () {
+    if ($(document).scrollTop() == 0) {
+      $(".navbar").removeClass("tiny");
+      $('.fixed-top').addClass("navbar-color");
+      $('.fixed-top').removeClass("navbar-transparent");
+    } else {
+      $('.fixed-top').addClass("navbar-transparent");
+      $('.fixed-top').css('transition', 'all 0.5s ease-in-out 0s');
+      $(".navbar").addClass("tiny");
+    }
+  });
 });
 
-$("#accordion").on("hide.bs.collapse show.bs.collapse", e => {
-    $(e.target)
-      .prev()
-      .find("i:last-child")
-      .toggleClass("fa-minus fa-plus");
-  });
-  
+$("#accordion").on("hide.bs.collapse show.bs.collapse", (e) => {
+  $(e.target).prev().find("i:last-child").toggleClass("fa-minus fa-plus");
+});
